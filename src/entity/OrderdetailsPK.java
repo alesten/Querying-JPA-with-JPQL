ab4ef945a62aa43219aa1c96077de5db856ffbb3
Alexander Steen <ale.steen@hotmail.com>
2015-09-18 09:48:41 +0200

    Added entity classes

---
+++ src/entity/OrderdetailsPK.java
@@ -0,0 +1,70 @@
+package entity; 
+
+import java.io.Serializable; 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class OrderdetailsPK implements Serializable { 
+    @Basic(optional = false)
+    @Column(name = "orderNumber")
+    private int orderNumber;
+    @Basic(optional = false)
+    @Column(name = "productCode")
+    private String productCode;
+
+    public OrderdetailsPK() {
+    }
+
+    public OrderdetailsPK(int orderNumber, String productCode) {
+        this.orderNumber = orderNumber;
+        this.productCode = productCode;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) orderNumber;
+        hash += (productCode != null ? productCode.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OrderdetailsPK)) {
+            return false;
+        }
+        OrderdetailsPK other = (OrderdetailsPK) object;
+        if (this.orderNumber != other.orderNumber) {
+            return false;
+        }
+        if ((this.productCode == null && other.productCode != null) || (this.productCode != null && !this.productCode.equals(other.productCode))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.OrderdetailsPK[ orderNumber=" + orderNumber + ", productCode=" + productCode + " ]";
+    }
+
+}
